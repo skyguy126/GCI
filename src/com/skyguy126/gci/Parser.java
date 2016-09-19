28935292bca5894149a8382c873973cab39745df
skyguy126 <vamsieyunni@gmail.com>
2016-09-18 22:26:03 -0700

    Added G00 Detection

--- src/com/skyguy126/gci/Parser.java
+++ src/com/skyguy126/gci/Parser.java
@@ -70,103 +70,188 @@ public class Parser {
 					break;
 				}
 
-				// Begin looping through each command
+				// Initialize array for loop
 				ArrayList<String> cmdList = new ArrayList<String>();
-				boolean exitLoop = false;
+
+				// Boolean operations are faster than string comparisons so we
+				// use short circuit evaluation
+				boolean lastCmdExists = false;
+
+				// we cannot use valid for this because even if we find an error
+				// we want to check the rest of the file
+				boolean loopExit = false;
+
+				// Keep track of cmds that need arguments
 				String lastCmd = "";
+
+				// This is especially necessary for move commands, do not accept
+				// blank move commands
+				boolean argsMet = false;
+
+				// Keep track of the command block number
 				int cmdNum = -1;
+
+				Logger.debug("Entering loop for line {}", lineNum);
 
 				// Remove \n and double spaces before processing
 				for (String x : currentLine.replaceAll("\n", "").replaceAll("\\s+", " ").split(" ")) {
+					// Trim trailing whitespace and increment command number
+					x = x.trim();
 					cmdNum++;
 
 					// Assert N tag at start of line
-					if (cmdNum == 0 && !Pattern.compile("[N]\\d+").matcher(x.trim()).matches()) {
+					if (cmdNum == 0 && !Pattern.compile("[N]\\d+").matcher(x).matches()) {
 						Logger.error("Invalid N tag at line {}", lineNum);
 						valid = false;
+						loopExit = true;
 					} else if (cmdNum == 1) {
 						// Command following N tag MUST be G or M
 
-						Matcher mg = Pattern.compile("[G]\\d+").matcher(x.trim());
-						Matcher mm = Pattern.compile("[M]\\d+").matcher(x.trim());
+						Matcher mg = Pattern.compile("[G]\\d+").matcher(x);
+						Matcher mm = Pattern.compile("[M]\\d+").matcher(x);
 
 						if (!mg.matches() && !mm.matches()) {
 							Logger.error("Syntax error on line {}. N tag must be followed by a proper G or M tag",
 									lineNum);
 							valid = false;
+							loopExit = true;
 						}
 					}
-					
+
+					// No need to process the N tag, ignoring that anyways
 					if (cmdNum == 0) {
 						continue;
 					}
-					
-					switch (x.trim()) {
-						case "G04":
-						case "G05":
-						case "G80":
-						case "G81":
-						case "G82":
-						case "M00":
-						case "M01":
-						case "M06":
-						case "M08":
-						case "M09":
-						case "M10":
-						case "M11":
-						case "M30":
-						case "M47":
-							break;
-						case "G20":
-							if (measurementMode == null) {
-								measurementMode = MeasurementMode.INCH;
-								Logger.debug("Measurement mode: in");
-							} else {
-								Logger.error("Measurement mode can only be defined once");
-								exitLoop = true;
-								valid = false;
-							}
-							break;
-						case "G21":
-							if (measurementMode == null) {
-								measurementMode = MeasurementMode.MILLIMETER;	
-								Logger.debug("Measurement mode: mm");
-							} else {
-								Logger.error("Measurement mode can only be defined once");
-								exitLoop = true;
-								valid = false;
-							}
-							break;
-						case "G90":
-							if (coordinateMode == null) {
-								coordinateMode = CoordinateMode.ABSOLUTE;	
-							} else {
-								Logger.error("Coordinate mode can only be defined once");
-								exitLoop = true;
-								valid = false;
-							}
-							break;
-						case "G91":
-							if (coordinateMode == null) {
-								coordinateMode = CoordinateMode.RELATIVE;	
-							} else {
-								Logger.error("Coordinate mode can only be defined once");
-								exitLoop = true;
-								valid = false;
-							}
-							break;
-						default:
-							Logger.error("Invalid code on line {}", lineNum);
-							exitLoop = true;
+
+					// Last command was M03 to start spindle and we need to get
+					// spindle speed
+					if (lastCmdExists && lastCmd.equals("M03")) {
+						if (Pattern.compile("[S]\\d+").matcher(x).matches()) {
+							Logger.debug("Found S value");
+							cmdList.add(x);
+							lastCmdExists = false;
+							lastCmd = "";
+							continue;
+						} else {
+							Logger.error("M03 must be followed by S tag indicating spindle speed on line {}", lineNum);
 							valid = false;
 							break;
+						}
 					}
 
-					if (exitLoop) {
+					if (lastCmdExists && lastCmd.equals("G00")) {
+						Logger.debug("Found G00 as last cmd");
+
+						// Search for X Y Z and F tags following G00
+						if (Pattern.compile("[X]\\d+([.]\\d+)").matcher(x).matches()) {
+							Logger.debug("Detected x val");
+							cmdList.add(x);
+							argsMet = true;
+							continue;
+						} else if (Pattern.compile("[Y]\\d+([.]\\d+)").matcher(x).matches()) {
+							Logger.debug("Detected y val");
+							cmdList.add(x);
+							argsMet = true;
+							continue;
+						} else if (Pattern.compile("[Z]\\d+([.]\\d+)").matcher(x).matches()) {
+							Logger.debug("Detected z val");
+							cmdList.add(x);
+							argsMet = true;
+							continue;
+						} else if (Pattern.compile("[F]\\d+").matcher(x).matches()) {
+							Logger.debug("Detected f val");
+							cmdList.add(x);
+							continue;
+						} else if (!argsMet) {
+							Logger.error("G00 must be supplied with X Y or Z tag on line {}", lineNum);
+							valid = false;
+							break;
+						}
+					}
+
+					switch (x) {
+					case "G04":
+					case "G05":
+					case "G80":
+					case "G81":
+					case "G82":
+					case "M00":
+					case "M01":
+					case "M06":
+					case "M08":
+					case "M09":
+					case "M10":
+					case "M11":
+					case "M30":
+					case "M47":
+						break;
+					case "M03":
+						Logger.debug("Set last cmd to M03");
+						cmdList.add(x);
+						lastCmdExists = true;
+						lastCmd = x;
+						break;
+					case "G20":
+						if (measurementMode == null) {
+							measurementMode = MeasurementMode.INCH;
+							Logger.debug("Measurement mode: inch");
+						} else {
+							Logger.error("Measurement mode can only be defined once");
+							valid = false;
+							loopExit = true;
+						}
+						break;
+					case "G21":
+						if (measurementMode == null) {
+							measurementMode = MeasurementMode.MILLIMETER;
+							Logger.debug("Measurement mode: mm");
+						} else {
+							Logger.error("Measurement mode can only be defined once");
+							valid = false;
+							loopExit = true;
+						}
+						break;
+					case "G90":
+						if (coordinateMode == null) {
+							coordinateMode = CoordinateMode.ABSOLUTE;
+							Logger.debug("Coordinate mode: absolute");
+						} else {
+							Logger.error("Coordinate mode can only be defined once");
+							valid = false;
+							loopExit = true;
+						}
+						break;
+					case "G91":
+						if (coordinateMode == null) {
+							coordinateMode = CoordinateMode.RELATIVE;
+							Logger.debug("Coordinate mode: relative");
+						} else {
+							Logger.error("Coordinate mode can only be defined once");
+							valid = false;
+							loopExit = true;
+						}
+						break;
+					case "G00":
+						Logger.debug("Set last cmd to G00");
+						cmdList.add(x);
+						lastCmdExists = true;
+						lastCmd = x;
+						break;
+					default:
+						Logger.error("Invalid code on line {} block {}", lineNum, cmdNum);
+						valid = false;
+						loopExit = true;
+						break;
+					}
+
+					if (loopExit) {
 						break;
 					}
 
 				}
+
+				System.out.println(cmdList);
+
 			}
 
 			this.fileReader.close();
