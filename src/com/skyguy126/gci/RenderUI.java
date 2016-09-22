0bcf7bbbc8269cc122be94d3c983c94abbfd3cba
skyguy126 <vamsieyunni@gmail.com>
2016-09-21 17:55:50 -0700

    Added basic opengl methods, added menubar

--- src/com/skyguy126/gci/RenderUI.java
+++ src/com/skyguy126/gci/RenderUI.java
@@ -1,7 +1,11 @@
 package com.skyguy126.gci;
 
 import java.awt.Frame;
+import java.awt.Menu;
 import java.awt.MenuBar;
+import java.awt.MenuItem;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 
@@ -14,16 +18,17 @@ import com.jogamp.opengl.GLCapabilities;
 import com.jogamp.opengl.GLEventListener;
 import com.jogamp.opengl.GLProfile;
 import com.jogamp.opengl.awt.GLCanvas;
+import com.jogamp.opengl.glu.GLU;
 import com.jogamp.opengl.util.Animator;
 
 public class RenderUI implements WindowListener, GLEventListener {
 	
-	Frame frame;
-	MenuBar menu;
+	private Frame frame;
+	private MenuBar menuBar;
 	
-	GLCapabilities glcaps;
-	GLCanvas glcanvas;
-	Animator painter;
+	private GLCapabilities glcaps;
+	private GLCanvas glcanvas;
+	private Animator animator;
 
 	public RenderUI() {
 		Configurator.defaultConfig().formatPattern(Shared.LOG_FORMAT).level(Shared.LOG_LEVEL).activate();
@@ -40,19 +45,39 @@ public class RenderUI implements WindowListener, GLEventListener {
 		
 		frame = new Frame("GCODE Interpreter - " + Shared.VERSION);
         frame.setSize(800, 800);
-        frame.setResizable(false);
+        frame.setResizable(true);
         frame.addWindowListener(this);        
 		frame.add(glcanvas);
 		
-		painter = new Animator(glcanvas);
-		painter.start();
+		menuBar = new MenuBar();
 		
+		Menu file = new Menu("File");
+		MenuItem exitMenuItem = new MenuItem("Exit");
+		exitMenuItem.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				exit();
+			}
+		});
+		
+		file.add(exitMenuItem);
+		menuBar.add(file);
+		
+		frame.setMenuBar(menuBar);
 		frame.setVisible(true);
 	}
 	
 	@Override
 	public void display(GLAutoDrawable glad) {
+		GL2 gl = glad.getGL().getGL2();
 		
+		gl.glClear(GL2.GL_COLOR_BUFFER_BIT | GL2.GL_DEPTH_BUFFER_BIT);
+		gl.glColor3f(0.9f, 0.5f, 0.2f);
+        gl.glBegin(GL2.GL_TRIANGLE_FAN);
+        gl.glVertex3f(-20, -20, 0);
+        gl.glVertex3f(+20, -20, 0);
+        gl.glVertex3f(0, 20, 0);
+        gl.glEnd();
 	}
 
 	@Override
@@ -65,12 +90,47 @@ public class RenderUI implements WindowListener, GLEventListener {
 	public void init(GLAutoDrawable glad) {
 		Logger.debug("OpenGL init");
 		
+		GL2 gl = glad.getGL().getGL2();
+		GLU glu = new GLU();
+		
+		gl.glEnable(GL2.GL_DEPTH_TEST);
+		gl.glDepthFunc(GL2.GL_LEQUAL);
+		gl.glShadeModel(GL2.GL_SMOOTH);
+		gl.glHint(GL2.GL_PERSPECTIVE_CORRECTION_HINT, GL2.GL_NICEST);
+		gl.glClearColor(0f, 0f, 0f, 1f);
+		
+		setCamera(gl, glu, 100);
+		
+		animator = new Animator(glcanvas);
+		animator.start();
 	}
 
 	@Override
-	public void reshape(GLAutoDrawable arg0, int arg1, int arg2, int arg3, int arg4) {
-		// TODO Auto-generated method stub
+	public void reshape(GLAutoDrawable glad, int x, int y, int width, int height) {
+		GL2 gl = glad.getGL().getGL2();
+		gl.glViewport(x, y, width, height);
 		
+		Logger.debug("GL reshape");
+	}
+	
+	public void setCamera(GL2 gl, GLU glu, float distance) {
+		gl.glMatrixMode(GL2.GL_PROJECTION);
+		gl.glLoadIdentity();
+		
+		float widthHeightRatio = (float) glcanvas.getWidth() / (float) glcanvas.getHeight();
+		glu.gluPerspective(45, widthHeightRatio, 1, 1000);
+		glu.gluLookAt(0, 0, distance, 0, 0, 0, 0, 1, 0);
+		
+		gl.glMatrixMode(GL2.GL_MODELVIEW);
+		gl.glLoadIdentity();
+	}
+	
+	public void exit() {
+		Logger.debug("Window closing...");
+		animator.stop();
+		frame.remove(glcanvas);
+		frame.dispose();
+		System.exit(0);
 	}
 
 	@Override
@@ -87,11 +147,7 @@ public class RenderUI implements WindowListener, GLEventListener {
 
 	@Override
 	public void windowClosing(WindowEvent evt) {
-		Logger.debug("Window closing...");
-		painter.stop();
-		frame.remove(glcanvas);
-		evt.getWindow().dispose();
-		System.exit(0);
+		exit();
 	}
 
 	@Override
